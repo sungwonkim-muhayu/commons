6f84d5beccd5e9a4e16b61c8fa3eb24095bb3f0a
swsz2 <76676913+swsz2@users.noreply.github.com>
2023-02-17 21:42:54 +0900

    [feat] 트랜잭션 핸들러 추가

---
+++ src/main/java/org/github/swsz2/commons/tx/TransactionHandler.java
@@ -0,0 +1,11 @@
+package org.github.swsz2.commons.tx;
+
+/** 익명 함수 또는 Task를 입력 받아 트랜잭션 관련 처리를 수행하는 인터페이스. */
+public interface TransactionHandler {
+
+  void execute(final Task task);
+
+  interface Task {
+    void submit();
+  }
+}
