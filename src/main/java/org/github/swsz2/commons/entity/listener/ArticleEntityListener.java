7a8b57f6f09d37703bbf838a54a2df63e173bd75
sungwonkim-muhayu <sungwonkim@muhayu.com>
2023-03-10 10:45:38 +0900

    [feat] entity listener 리플랙션 예제 추가

--- src/main/java/org/github/swsz2/commons/entity/listener/ArticleEntityListener.java
+++ src/main/java/org/github/swsz2/commons/entity/listener/ArticleEntityListener.java
@@ -1,18 +1,30 @@
 package org.github.swsz2.commons.entity.listener;
 
+import lombok.SneakyThrows;
 import org.github.swsz2.commons.entity.Article;
 import org.springframework.util.Assert;
+import org.springframework.util.ReflectionUtils;
 
 import javax.persistence.PrePersist;
+import java.lang.reflect.Field;
 
 public class ArticleEntityListener {
 
 
+    @SneakyThrows
     @PrePersist
     public void prePersist(Object target) {
         Assert.notNull(target, "Entity must not be null!");
         if (target instanceof Article) {
+            // 객체 매핑해서 하거나 (setter 있을 때)
             ((Article) target).setTitle("default title!");
+
+            // 리플렉션으로 처리하거나 (setter 없을 때)
+            final Field field = ReflectionUtils.findField(target.getClass(), "title");
+            Assert.notNull(field, "Entity field not be null!");
+
+            field.setAccessible(true);
+            field.set(target, "default title!");
         }
     }
 }
